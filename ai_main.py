4eb649551c53c49c8c4910f454382a6d7a779ccb
Buckmaster <sean.buckmaster@gmail.com>
2023-10-08 17:53:06 -0400

    Improved stopping mechanism in ai_main.py

--- ai_main.py
+++ ai_main.py
@@ -2,36 +2,45 @@
 import threading
 import time
 
-def main_loop():
+def main_loop(stop_event):
     while not stop_event.is_set():
-        # Your main loop logic here
-        time.sleep(2)  # Using ChatGPT 3.5 every 2 seconds
+        # Your main loop code here
+        print("|", end="", flush=True)
+        for _ in range(60):  # Check the stop event every second for a minute
+            if stop_event.is_set():
+                break
+            time.sleep(1)
 
-def check_stop():
-    while True:
-        user_input = input("\nYou can type 'stop' to stop AI Main: ")
-        if user_input.strip().lower() == 'stop':
-            print("Stopping AI Main...")
-            stop_event.set()
-            break
-
-def management_tasks():
+def management_tasks(stop_event):
     while not stop_event.is_set():
-        # Your management and verification logic here
-        time.sleep(60)  # Using ChatGPT 4.0 every 60 seconds
+        # Your management tasks code here
+        print("/", end="", flush=True)
+        for _ in range(30):  # Check the stop event every second for 30 seconds
+            if stop_event.is_set():
+                break
+            time.sleep(1)
 
-stop_event = threading.Event()
+def start_ai():
+    stop_event = threading.Event()
+    main_thread = threading.Thread(target=main_loop, args=(stop_event,))
+    management_thread = threading.Thread(target=management_tasks, args=(stop_event,))
 
-main_thread = threading.Thread(target=main_loop)
-stop_thread = threading.Thread(target=check_stop)
-management_thread = threading.Thread(target=management_tasks)
+    main_thread.start()
+    management_thread.start()
 
-print("AI Main is running. Monitoring tasks and awaiting user input...")
+    try:
+        while True:
+            user_input = input("\nYou can type 'stop' to stop AI Main: ")
+            if user_input.lower() == 'stop':
+                print("Stopping AI Main...")
+                stop_event.set()
+                break
+    except KeyboardInterrupt:
+        print("Stopping AI Main...")
+        stop_event.set()
 
-main_thread.start()
-stop_thread.start()
-management_thread.start()
+    main_thread.join()
+    management_thread.join()
 
-main_thread.join()
-stop_thread.join()
-management_thread.join()
+if __name__ == "__main__":
+    start_ai()
--- update.py
+++ update.py
@@ -1,52 +1,65 @@
-import os
+# update.py
 
-# Update the ai_main.py
+import os
+import time
+
+# Updating the ai_main.py with improved stopping mechanism
 ai_main_code = """
 import threading
 import time
 
-def main_loop():
+def main_loop(stop_event):
     while not stop_event.is_set():
-        # Your main loop logic here
-        time.sleep(2)  # Using ChatGPT 3.5 every 2 seconds
+        # Your main loop code here
+        print("|", end="", flush=True)
+        for _ in range(60):  # Check the stop event every second for a minute
+            if stop_event.is_set():
+                break
+            time.sleep(1)
 
-def check_stop():
-    while True:
-        user_input = input("\\nYou can type 'stop' to stop AI Main: ")
-        if user_input.strip().lower() == 'stop':
-            print("Stopping AI Main...")
-            stop_event.set()
-            break
-
-def management_tasks():
+def management_tasks(stop_event):
     while not stop_event.is_set():
-        # Your management and verification logic here
-        time.sleep(60)  # Using ChatGPT 4.0 every 60 seconds
+        # Your management tasks code here
+        print("/", end="", flush=True)
+        for _ in range(30):  # Check the stop event every second for 30 seconds
+            if stop_event.is_set():
+                break
+            time.sleep(1)
 
-stop_event = threading.Event()
+def start_ai():
+    stop_event = threading.Event()
+    main_thread = threading.Thread(target=main_loop, args=(stop_event,))
+    management_thread = threading.Thread(target=management_tasks, args=(stop_event,))
 
-main_thread = threading.Thread(target=main_loop)
-stop_thread = threading.Thread(target=check_stop)
-management_thread = threading.Thread(target=management_tasks)
+    main_thread.start()
+    management_thread.start()
 
-print("AI Main is running. Monitoring tasks and awaiting user input...")
+    try:
+        while True:
+            user_input = input("\\nYou can type 'stop' to stop AI Main: ")
+            if user_input.lower() == 'stop':
+                print("Stopping AI Main...")
+                stop_event.set()
+                break
+    except KeyboardInterrupt:
+        print("Stopping AI Main...")
+        stop_event.set()
 
-main_thread.start()
-stop_thread.start()
-management_thread.start()
+    main_thread.join()
+    management_thread.join()
 
-main_thread.join()
-stop_thread.join()
-management_thread.join()
+if __name__ == "__main__":
+    start_ai()
 """
 
+# Writing the updated code to ai_main.py
 with open("ai_main.py", "w") as file:
     file.write(ai_main_code)
 
 # Git commands to add, commit, and push changes to GitHub
 os.system("git add .")
-os.system('git commit -m "Updated ai_main.py with non-intrusive running message"')
+os.system('git commit -m "Improved stopping mechanism in ai_main.py"')
 os.system("git push origin main")
 
 # Informing the user about the update completion
-print("Update completed! ai_main.py has been updated and changes have been pushed to GitHub.")
+print("Update completed! The stopping mechanism in ai_main.py has been improved and pushed to GitHub.")
