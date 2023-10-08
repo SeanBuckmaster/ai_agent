0290bdac55320946c8096fa5568402c1dae9251a
Buckmaster <sean.buckmaster@gmail.com>
2023-10-08 15:34:24 -0400

    Refactoring: Organized files into layers and added execution log

--- update.py
+++ update.py
@@ -1,35 +1,36 @@
 import os
+import logging
 import shutil
 
-# Ensure to backup your project before running this script
+# Configure logging
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    handlers=[logging.FileHandler("Logs/execution.log", 'a', 'utf-8'),
+                              logging.StreamHandler()])
 
 # Directories to organize files
 directories = {
-    "BusinessLayer": ["ai_tasks.py", "ai_goals.py", "ai_errors.py", "ai_task_processor.py"],
-    "DataLayer": ["ai_data_read.py", "ai_data_write.py"],
-    "PresentationLayer": ["ai_interface.py", "ai_chatgpt.py"],
-    "CommonLayer": ["ai_xml_read.py", "ai_xml_write.py", "ai_xml_handler.py", "ai_error_handler.py"]
+    "BusinessLayer": ["ai_task_processor.py"],
+    "CommonLayer": ["ai_error_handler.py", "ai_xml_handler.py"]
 }
 
 # Create directories and move files
 for dir_name, files in directories.items():
     os.makedirs(dir_name, exist_ok=True)
     for file_name in files:
-        if os.path.exists(file_name):
+        try:
             shutil.move(file_name, os.path.join(dir_name, file_name))
-        else:
-            print(f"Warning: {file_name} does not exist and was not moved.")
-
-# Initialize Git if not already done
-if not os.path.exists(".git"):
-    os.system('git init')
+            logging.info(f"Moved {file_name} to {dir_name}")
+        except FileNotFoundError:
+            logging.warning(f"{file_name} does not exist and was not moved.")
 
 # Committing the changes to Git
 os.system('git add .')
-os.system('git commit -m "Refactoring: Organized files into layers"')
+os.system(f'git commit -m "Refactoring: Organized files into layers and added execution log"')
+os.system('git push')
 
 # Erasing the content of update.py after execution
 with open("update.py", "w") as f:
     f.write("# This script has been executed and is now empty to prevent re-execution.\n")
 
-print("Update completed! Files have been organized into respective directories.")
+logging.info("Update completed! Files have been organized into respective directories.")
