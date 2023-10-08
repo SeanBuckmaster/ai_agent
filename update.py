d1e3c672e06947c46cb6d28ddc74e9e78b05ff59
Buckmaster <sean.buckmaster@gmail.com>
2023-10-08 15:53:10 -0400

    Initialized new XML log files

--- update.py
+++ update.py
@@ -1,20 +1,35 @@
-# update.py
-
 import os
-import shutil
+import xml.etree.ElementTree as ET
+from datetime import datetime
 
-# Rename log files to .xml if they exist
-if os.path.exists("Logs/update_log.txt"):
-    os.rename("Logs/update_log.txt", "Logs/update_log.xml")
+# Delete old log files if they exist
+if os.path.exists("Logs/update_log.xml"):
+    os.remove("Logs/update_log.xml")
 
-if os.path.exists("Logs/error_log.txt"):
-    os.rename("Logs/error_log.txt", "Logs/error_log.xml")
+if os.path.exists("Logs/error_log.xml"):
+    os.remove("Logs/error_log.xml")
+
+# Create a new XML log file with a timestamp and a log entry
+def create_log_entry(filename, message):
+    root = ET.Element("log")
+    entry = ET.SubElement(root, "entry")
+    timestamp = ET.SubElement(entry, "timestamp")
+    timestamp.text = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    log_message = ET.SubElement(entry, "message")
+    log_message.text = message
+    
+    tree = ET.ElementTree(root)
+    tree.write(filename)
+
+# Create log entries
+os.makedirs("Logs", exist_ok=True)
+create_log_entry("Logs/update_log.xml", "Log file created. Previous log files were deleted.")
+create_log_entry("Logs/error_log.xml", "Error log file created. Previous error log files were deleted.")
 
 # Git commands to add, commit, and push changes to GitHub
 os.system("git add .")
-os.system('git commit -m "Changed log files to XML format"')
+os.system('git commit -m "Initialized new XML log files"')
 os.system("git push origin main")
 
 # Informing the user about the update completion
-print("Update completed! Log files have been changed to XML format and pushed to GitHub.")
-
+print("Update completed! New XML log files have been initialized and pushed to GitHub.")
