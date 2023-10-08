2d0b1670175845d47c544e6b6c7319886ea8dead
Buckmaster <sean.buckmaster@gmail.com>
2023-10-08 18:09:27 -0400

    Organized main_loop.py and verified import in ai_main.py

--- update.py
+++ update.py
@@ -1,10 +1,9 @@
-# This script has been executed and is now empty to prevent re-execution.
 import os
 import shutil
 import xml.etree.ElementTree as ET
 from datetime import datetime
 
-# Function to create a log entry in XML format
+# Function to create a log entry
 def create_log_entry(filename, message):
     if os.path.exists(filename):
         tree = ET.parse(filename)
@@ -21,27 +20,32 @@ def create_log_entry(filename, message):
     tree = ET.ElementTree(root)
     tree.write(filename)
 
-# Convert stop_flag.txt to XML and move to DataLayer
-if os.path.exists("stop_flag.txt"):
-    os.remove("stop_flag.txt")
-
-stop_flag_root = ET.Element("stop_flag")
-flag = ET.SubElement(stop_flag_root, "flag")
-flag.text = "False"
-tree = ET.ElementTree(stop_flag_root)
-tree.write("DataLayer/stop_flag.xml")
-
 # Move main_loop.py to BusinessLayer
 if os.path.exists("main_loop.py"):
-    shutil.move("main_loop.py", "BusinessLayer/main_loop.py")
+    shutil.move("main_loop.py", os.path.join("BusinessLayer", "main_loop.py"))
+    create_log_entry("Logs/update_log.xml", "Moved main_loop.py to BusinessLayer.")
+else:
+    create_log_entry("Logs/error_log.xml", "main_loop.py does not exist in the root directory.")
+
+# Check if ai_main.py is calling main_loop.py from BusinessLayer
+try:
+    with open("ai_main.py", "r") as file:
+        content = file.read()
+        if 'from BusinessLayer import main_loop' not in content:
+            create_log_entry("Logs/error_log.xml", "ai_main.py is not importing main_loop.py from BusinessLayer.")
+        else:
+            create_log_entry("Logs/update_log.xml", "ai_main.py is correctly importing main_loop.py from BusinessLayer.")
+except FileNotFoundError:
+    create_log_entry("Logs/error_log.xml", "ai_main.py does not exist in the root directory.")
 
 # Git commands to add, commit, and push changes to GitHub
 os.system("git add .")
-os.system('git commit -m "Organized files and converted stop_flag to XML"')
+os.system('git commit -m "Organized main_loop.py and verified import in ai_main.py"')
 os.system("git push origin main")
 
-# Log the update
-create_log_entry("Logs/update_log.xml", "Organized files and converted stop_flag to XML. Moved main_loop.py to BusinessLayer.")
-
 # Informing the user about the update completion
-print("Update completed! Files have been organized, stop_flag.txt has been converted to XML, and main_loop.py has been moved to BusinessLayer.")
+print("Update completed! main_loop.py has been organized and ai_main.py import verified. Check the logs for details.")
+
+# Erasing the content of update.py after execution
+with open("update.py", "w") as f:
+    f.write("# This script has been executed and is now empty to prevent re-execution.\n")
