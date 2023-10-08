3d92d7b00d332c94838e330435dda84bd03e01cd
Buckmaster <sean.buckmaster@gmail.com>
2023-10-08 15:46:35 -0400

    Update: Adding all files and folders

--- update.py
+++ update.py
@@ -1,36 +1,16 @@
 import os
-import logging
-import shutil
 
-# Configure logging
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(levelname)s - %(message)s',
-                    handlers=[logging.FileHandler("Logs/execution.log", 'a', 'utf-8'),
-                              logging.StreamHandler()])
-
-# Directories to organize files
-directories = {
-    "BusinessLayer": ["ai_task_processor.py"],
-    "CommonLayer": ["ai_error_handler.py", "ai_xml_handler.py"]
-}
-
-# Create directories and move files
-for dir_name, files in directories.items():
-    os.makedirs(dir_name, exist_ok=True)
-    for file_name in files:
-        try:
-            shutil.move(file_name, os.path.join(dir_name, file_name))
-            logging.info(f"Moved {file_name} to {dir_name}")
-        except FileNotFoundError:
-            logging.warning(f"{file_name} does not exist and was not moved.")
-
-# Committing the changes to Git
+# Add all files to the local Git repository
 os.system('git add .')
-os.system(f'git commit -m "Refactoring: Organized files into layers and added execution log"')
-os.system('git push')
+
+# Commit the changes with a message
+os.system('git commit -m "Update: Adding all files and folders"')
+
+# Push the changes to GitHub
+os.system('git push origin main')
 
 # Erasing the content of update.py after execution
 with open("update.py", "w") as f:
     f.write("# This script has been executed and is now empty to prevent re-execution.\n")
 
-logging.info("Update completed! Files have been organized into respective directories.")
+print("Update completed! Files and folders have been added, committed, and pushed to GitHub.")
